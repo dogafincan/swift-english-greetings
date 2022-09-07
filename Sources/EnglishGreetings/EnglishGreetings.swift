5f0b96fea9506765d53535d042d9f9d668dc7e6a
Doga Fincan <doga@icloud.com>
2022-09-07 15:27:59 +0200

    Add first version of EnglishGreetings

--- Sources/EnglishGreetings/EnglishGreetings.swift
+++ Sources/EnglishGreetings/EnglishGreetings.swift
@@ -1,6 +1,36 @@
-public struct EnglishGreetings {
-    public private(set) var text = "Hello, World!"
+import Gen
 
-    public init() {
+var xoshiro = Xoshiro()
+
+public enum NameError: Error {
+    case EmptyNameArgument(String)
+    case EmptyNamesArgument(String)
+}
+
+public func randomEnglishGreeting(_ name: String) throws -> String {
+    guard !name.isEmpty else {
+        throw NameError.EmptyNameArgument("expected a non-empty string")
     }
+    
+    let greetings = [
+        "Hi, \(name). Welcome!",
+        "Great to see you, \(name)!",
+        "Hail, \(name)! Well met!",
+    ]
+    
+    return Gen.element(of: greetings).run(using: &xoshiro)!
+}
+
+public func randomEnglishGreetings(_ names: String...) throws -> [String] {
+    guard !names.isEmpty else {
+        throw NameError.EmptyNamesArgument("expected at least one name")
+    }
+    
+    var greetings = [String]()
+    
+    for name in names {
+        greetings.append(try randomEnglishGreeting(name))
+    }
+    
+    return greetings
 }
--- Tests/EnglishGreetingsTests/EnglishGreetingsTests.swift
+++ Tests/EnglishGreetingsTests/EnglishGreetingsTests.swift
@@ -1,11 +1,48 @@
+import Gen
 import XCTest
 @testable import EnglishGreetings
 
 final class EnglishGreetingsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(EnglishGreetings().text, "Hello, World!")
+    func testrandomEnglishGreeting() throws {
+        xoshiro = Xoshiro(seed: 0)
+        XCTAssertEqual(try randomEnglishGreeting("Doga"), "Hi, Doga. Welcome!")
+    }
+    
+    func testrandomEnglishGreetingEmptyString() throws {
+        XCTAssertThrowsError(try randomEnglishGreeting("")) { error in
+            guard case NameError.EmptyNameArgument(let message) = error else {
+                return XCTFail()
+            }
+            
+            XCTAssertEqual(message, "expected a non-empty string")
+        }
+    }
+    
+    func testrandomEnglishGreetings() throws {
+        xoshiro = Xoshiro(seed: 0)
+        XCTAssertEqual(
+            try randomEnglishGreetings("Ji-an", "Hiroto", "Somchai"),
+            ["Hi, Ji-an. Welcome!", "Hi, Hiroto. Welcome!", "Great to see you, Somchai!"]
+        )
+    }
+    
+    func testrandomEnglishGreetingsNoArguments() throws {
+        XCTAssertThrowsError(try randomEnglishGreetings()) { error in
+            guard case NameError.EmptyNamesArgument(let message) = error else {
+                return XCTFail()
+            }
+            
+            XCTAssertEqual(message, "expected at least one name")
+        }
+    }
+    
+    func testrandomEnglishGreetingsEmptyString() throws {
+        XCTAssertThrowsError(try randomEnglishGreetings("")) { error in
+            guard case NameError.EmptyNameArgument(let message) = error else {
+                return XCTFail()
+            }
+            
+            XCTAssertEqual(message, "expected a non-empty string")
+        }
     }
 }
