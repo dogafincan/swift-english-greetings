d7ed9c7c8f29536f63e76fc453f4e8995acb9289
Doga Fincan <doga@icloud.com>
2022-09-07 08:47:50 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EnglishGreetings
+
+A description of this package.
---
+++ Sources/EnglishGreetings/EnglishGreetings.swift
@@ -0,0 +1,6 @@
+public struct EnglishGreetings {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
